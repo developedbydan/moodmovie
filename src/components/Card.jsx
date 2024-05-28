63741b1e4321a9f6321f973d6fd65f983d79ab07
Danijel <developedbydane@gmail.com>
2024-05-28 23:31:48 +0200

    feat(generate): generate movies

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,22 +4,34 @@ import happy from "../assets/happy.png";
 import sad from "../assets/sad.png";
 import amazing from "../assets/amazing.png";
 
-const Card = () => {
+const Card = ({ handleMood }) => {
   return (
-    <div className="grid grid-cols-2 gap-5 mt-10 md:grid-cols-4 md:gap-10 ">
-      <div className="flex flex-col items-center  p-5 rounded-2xl bg-orange-888 w-full h-52">
+    <div className="grid grid-cols-2 gap-5 mt-10 md:grid-cols-4 md:gap-10">
+      <div
+        className="flex flex-col items-center p-5 rounded-2xl bg-orange-888 w-full h-52 cursor-pointer"
+        onClick={() => handleMood("angry")}
+      >
         <h3 className="text-xl font-bold text-orange-999">Angry</h3>
         <img src={angry} alt="angry" className="w-36 mt-4" />
       </div>
-      <div className="flex flex-col items-center  p-5 rounded-2xl bg-blue-888 w-full h-52">
+      <div
+        className="flex flex-col items-center p-5 rounded-2xl bg-blue-888 w-full h-52 cursor-pointer"
+        onClick={() => handleMood("happy")}
+      >
         <h3 className="text-xl font-bold text-blue-999">Happy</h3>
         <img src={happy} alt="happy" className="w-36 mt-4" />
       </div>
-      <div className="flex flex-col items-center  p-5 rounded-2xl bg-green-888 w-full h-52">
+      <div
+        className="flex flex-col items-center p-5 rounded-2xl bg-green-888 w-full h-52 cursor-pointer"
+        onClick={() => handleMood("sad")}
+      >
         <h3 className="text-xl font-bold text-green-999">Sad</h3>
         <img src={sad} alt="sad" className="w-36 mt-4" />
       </div>
-      <div className="flex flex-col items-center  p-5 rounded-2xl bg-pink-888 w-full h-52">
+      <div
+        className="flex flex-col items-center p-5 rounded-2xl bg-pink-888 w-full h-52 cursor-pointer"
+        onClick={() => handleMood("amazing")}
+      >
         <h3 className="text-xl font-bold text-pink-999">Amazing</h3>
         <img src={amazing} alt="amazing" className="w-36 mt-4" />
       </div>
