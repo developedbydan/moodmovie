f4cc8f89aec4ada22f230b270644b28f1f542a21
Danijel <developedbydane@gmail.com>
2024-05-31 16:19:13 +0200

    feat(movie): movie info

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -6,6 +6,7 @@ const Movie = () => {
   const [movieInfo, setMovieInfo] = useState();
   const { id } = useParams();
   const apiKey = import.meta.env.VITE_RAPID_API_KEY;
+  const initialized = useRef(false);
 
   const getMovie = async () => {
     const options = {
@@ -25,7 +26,6 @@ const Movie = () => {
     try {
       const res = await axios.request(options);
       const movieData = res.data;
-      console.log(movieData);
       setMovieInfo(movieData);
     } catch (err) {
       console.log("Error fetching movies: ", err);
@@ -33,19 +33,40 @@ const Movie = () => {
   };
 
   useEffect(() => {
-    getMovie();
+    if (!initialized.current) {
+      initialized.current = true;
+      getMovie();
+      console.log("pokrenuo se");
+    }
   }, []);
 
   return (
-    <div>
+    <div className="px-7 mt-4 text-white">
       {movieInfo ? (
         <div>
-          <h1>{movieInfo.title}</h1>
-          <p>{movieInfo.overview}</p>
           <img
-            src={movieInfo.imageSet.verticalPoster.w240}
+            src={movieInfo.imageSet.horizontalPoster.w480}
             alt={movieInfo.title}
+            className="rounded-lg h-auto"
           />
+          <div className="pl-1">
+            <div className="mt-3 flex gap-4 items-center">
+              <h2 className="text-2xl font-bold ">{movieInfo.title}</h2>
+              <h4>({movieInfo.releaseYear})</h4>
+            </div>
+            <div className="flex gap-3 items-center">
+              {movieInfo.genres.map((genre) => (
+                <p
+                  key={genre.id}
+                  className="bg-gray-900 px-3 py-1 rounded-lg text-sm mt-4"
+                >
+                  {genre.name}
+                </p>
+              ))}
+            </div>
+            <h3 className="text-xl font-bold mt-9 mb-4">About</h3>
+            <p>{movieInfo.overview}</p>
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
